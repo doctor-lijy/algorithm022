8f03a9ff5c8c5f0151bf230791a43ce6dc346a94
李静毅 <517334391@qq.com>
2021-01-04 23:43:16 +0800

    有效的括号-20

---
+++ Week_01/有效的括号-20/有效的括号-20/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  有效的括号-20
+//
+//  Created by YJLi on 2021/1/4.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+    
+public:
+    bool isValid(string s) {
+
+        return true;
+        }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "===========\n";
+    
+    std::cout << "===========\n";
+    return 0;
+}
