b26f931c748037c063caaba67277dbc75615d9c2
李静毅 <517334391@qq.com>
2021-01-19 01:54:37 +0800

    快乐数-202

---
+++ Week_04/快乐数-202/快乐数-202/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  快乐数-202
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
