b3a57d54594ae327aec71360f5f05e7f3ea57311
李静毅 <517334391@qq.com>
2021-01-18 23:57:43 +0800

    平衡二叉树-110

---
+++ Week_02/平衡二叉树-110/平衡二叉树-110/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  平衡二叉树-110
+//
+//  Created by YJLi on 2021/1/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
+
+//一颗平衡二叉树的定义为：一个二叉树的每个节点的左右两个子树的高度差的绝对值不超过1
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
