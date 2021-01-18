5cc8e301124aca79bce8a3069533e8a670f6703a
李静毅 <517334391@qq.com>
2021-01-19 00:54:02 +0800

    删除二叉搜索树中的节点-450

---
+++ Week_03/删除二叉搜索树中的节点-450/删除二叉搜索树中的节点-450/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  删除二叉搜索树中的节点-450
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
