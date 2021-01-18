ba58636a5a2c59d9434a6cc2a9a64f60c0ad9f0a
李静毅 <517334391@qq.com>
2021-01-18 23:40:21 +0800

    完全二叉树的节点个数-222

---
+++ Week_02/完全二叉树的节点个数-222/完全二叉树的节点个数-222/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  完全二叉树的节点个数-222
+//
+//  Created by YJLi on 2021/1/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
+
+//
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
