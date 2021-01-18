f494e23c4a68cdf89721c9ca44b182ad3fb0c280
李静毅 <517334391@qq.com>
2021-01-19 00:56:00 +0800

    修剪二叉搜索树-669

---
+++ Week_03/修建二叉搜索树-669/修建二叉搜索树-669/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  修建二叉搜索树-669
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
