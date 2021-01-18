8d87789376e3e1cd3b046926da567a27d1f4c3de
李静毅 <517334391@qq.com>
2021-01-18 23:32:27 +0800

    二叉树最大的深度-104

---
+++ Week_02/二叉树最大的深度-104/二叉树最大的深度-104/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  二叉树最大的深度-104
+//
+//  Created by YJLi on 2021/1/18.
+//
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
+
+//二叉树的深度为根节点到最远叶子节点的最长路径上的节点数
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
