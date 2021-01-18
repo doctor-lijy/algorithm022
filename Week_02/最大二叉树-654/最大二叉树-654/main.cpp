3bd3c3e19bc1b731df9918f8cf5acd306ba52be7
李静毅 <517334391@qq.com>
2021-01-19 00:40:03 +0800

    合并二叉树

---
+++ Week_02/最大二叉树-654/最大二叉树-654/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  最大二叉树-654
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
