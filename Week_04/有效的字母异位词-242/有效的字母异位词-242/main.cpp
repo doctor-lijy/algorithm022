c6a56d4739a624ba2a15f00e799107db7d716870
李静毅 <517334391@qq.com>
2021-01-19 01:45:32 +0800

    有效的字母异位词-242

---
+++ Week_04/有效的字母异位词-242/有效的字母异位词-242/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  有效的字母异位词-242
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+//字母异位词指字母相同，但排列不同的字符串
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
