9ce3ff8e3273e144384c5ee4368b23b1a0b4a798
李静毅 <517334391@qq.com>
2021-01-19 00:45:23 +0800

    验证二叉搜索树-98

---
+++ Week_03/验证二叉搜索树-98/验证二叉搜索树-98/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  验证二叉搜索树-98
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
