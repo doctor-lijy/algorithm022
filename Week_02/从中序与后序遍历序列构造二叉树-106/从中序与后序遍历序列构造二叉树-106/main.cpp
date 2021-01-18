75e5d6318c9b7238a8dce62d517d8036208e6837
李静毅 <517334391@qq.com>
2021-01-19 00:36:34 +0800

    从中序与后序遍历序列构造二叉树-106

---
+++ Week_02/从中序与后序遍历序列构造二叉树-106/从中序与后序遍历序列构造二叉树-106/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  从中序与后序遍历序列构造二叉树-106
+//
+//  Created by YJLi on 2021/1/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
