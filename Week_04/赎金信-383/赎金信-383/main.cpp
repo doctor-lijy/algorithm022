3ec2b8b66b4f9f24cbe1a8c02c8f68cc45d7deac
李静毅 <517334391@qq.com>
2021-01-22 00:08:11 +0800

    赎金信-383

--- Week_04/赎金信-383/赎金信-383/main.cpp
+++ Week_04/赎金信-383/赎金信-383/main.cpp
@@ -6,6 +6,27 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    
+public:
+    bool canConstruct(string ransomNote, string magazine) { {
+        int record[26] = {0};
+        for (int i = 0; i < magazine.length(); i++) {
+            record[magazine[i] - 'a']++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++) {
+            record[ransomNote[j]-'a']--;
+            if (record[ransomNote[j]-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
