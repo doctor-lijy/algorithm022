d44d28a33fc912b7fbf68992587dde26c87f0aaf
李静毅 <517334391@qq.com>
2021-01-22 00:23:03 +0800

    这个需要好好在总结一下，代码重新再写一遍

--- Week_04/三数之和-15/三数之和-15/main.cpp
+++ Week_04/三数之和-15/三数之和-15/main.cpp
@@ -6,6 +6,26 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    
+public:
+    vector<vector<int>>threeSum(vector<int> &nums) {
+        vector<vector<int>>result;
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            
+            
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
