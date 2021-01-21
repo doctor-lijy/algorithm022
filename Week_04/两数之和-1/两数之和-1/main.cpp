eebcff08d1862cd435aa65bff88946c7cbf99aa1
李静毅 <517334391@qq.com>
2021-01-21 23:21:00 +0800

    两数之和

--- Week_04/两数之和-1/两数之和-1/main.cpp
+++ Week_04/两数之和-1/两数之和-1/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+            std::unordered_map <int,int> map;
+            for(int i = 0; i < nums.size(); i++) {
+                auto iter = map.find(target - nums[i]);
+                if(iter != map.end()) {
+                    return {iter->second, i};
+                }
+                map.insert(make_pair(nums[i], i));
+            }
+            return {};
+        }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "============\n";
+    Solution solu;
+    vector<int> vec = {2, 7, 11, 15};
+    vector<int> result = solu.twoSum(vec, 9);
+    for (int i = 0; i < result.size(); i++) {
+        cout<<result.at(i)<<" ";
+    }
+    cout<<endl;
+    
+    std::cout << "============\n";
     return 0;
 }
