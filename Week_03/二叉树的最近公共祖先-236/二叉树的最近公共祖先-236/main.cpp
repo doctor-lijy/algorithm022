000586978b2e5b269e0345caea622471879d63f3
李静毅 <517334391@qq.com>
2021-01-25 17:06:57 +0800

    二叉树的最近公共祖先-236

--- Week_03/二叉树的最近公共祖先-236/二叉树的最近公共祖先-236/main.cpp
+++ Week_03/二叉树的最近公共祖先-236/二叉树的最近公共祖先-236/main.cpp
@@ -16,6 +16,27 @@ struct TreeNode {
     TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
 };
 
+class Solution {
+    
+public:
+    
+    /*
+     思想有点不集中，没看明白这个回溯到底是怎么回事
+     */
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == q || root == p || root == NULL) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left != NULL && right != NULL) return root;
+        
+        if (left == NULL && right != NULL) return right;
+        else if (left != NULL && right != NULL) return left;
+        else {//  (left == NULL && right == NULL)
+            return NULL;
+        }
+    }
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
