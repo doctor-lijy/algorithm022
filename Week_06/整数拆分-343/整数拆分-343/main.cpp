9c07f97ec123ff6b0302927e152be21f38df7b4d
李静毅 <517334391@qq.com>
2021-01-28 19:44:42 +0800

    整数拆分-343

---
+++ Week_06/整数拆分-343/整数拆分-343/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  整数拆分-343
+//
+//  Created by YJLi on 2021/1/28.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ 给定一个正整数n，将其拆分为至少两个正整数的和，并使这些正整数的乘积最大化，返回你可以获得的最大乘积
+ */
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
