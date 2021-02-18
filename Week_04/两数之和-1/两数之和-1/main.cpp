d7a5ca1e50fc24ff200a4f3c995769a1c14cc7e1
李静毅 <517334391@qq.com>
2021-02-18 16:03:10 +0800

    两数之和-1

--- Week_04/两数之和-1/两数之和-1/main.cpp
+++ Week_04/两数之和-1/两数之和-1/main.cpp
@@ -11,20 +11,39 @@
 
 using namespace std;
 
+/*
+ 给定一个整数数组 nums 和一个目标值 target，在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ 
+ 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+ 「示例:」
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+ */
+
 class Solution {
     
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-            std::unordered_map <int,int> map;
-            for(int i = 0; i < nums.size(); i++) {
-                auto iter = map.find(target - nums[i]);
-                if(iter != map.end()) {
-                    return {iter->second, i};
-                }
-                map.insert(make_pair(nums[i], i));
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            /*
+             find(key)
+             查找以值为 key 的元素，如果找到，则返回一个指向该元素的正向迭代器；
+             反之，则返回一个指向容器中最后一个元素之后位置的迭代器（如果 end() 方法返回的迭代器）
+             
+             unordered_map<int, int>::iterator iter = map.find(target - nums[i]);
+             */
+            auto iter = map.find(target - nums[i]); //这种写法等同于上面那种写法
+            if (iter != map.end()) {
+                return {iter->second, i};
             }
-            return {};
+            map.insert(nums[i], i);
         }
+        return {};
+    }
 };
 
 int main(int argc, const char * argv[]) {
