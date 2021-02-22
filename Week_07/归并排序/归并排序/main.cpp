cd1ea80754527f78aead162f392e538d26ae0cbf
李静毅 <517334391@qq.com>
2021-02-22 14:35:06 +0800

    快速排序、归并排序

--- Week_07/归并排序/归并排序/main.cpp
+++ Week_07/归并排序/归并排序/main.cpp
@@ -6,6 +6,23 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+ 归并排序 Merge Sort- 分治
+ 
+ 1、把长度为n的输入序列分为两个长度为n/2的子序列
+ 2、对这两个子序列分别采用归并排序
+ 3、将两个排序好的子序列合并成一个最终的排序序列
+ */
+
+class Solution {
+    
+public:
+    
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
--- Week_07/快速排序/快速排序/main.cpp
+++ Week_07/快速排序/快速排序/main.cpp
@@ -19,14 +19,32 @@ using namespace std;
 /*
  快速排序的基本思想：
  通过一趟待排序记录分割成独立的两部分，其中一部分记录的关键字均比另一部分记录的关键字小，则可分别对这部分记录继续进行排序，以达到整个序列有序的目的
+ 
+ 数组取标杆pivot，将小元素放pivot左边，大元素放pivot右边，以此对右边和左边的子数组继续快排，以达到整个序列有序
  */
 class Solution {
     
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+    
+    int partition(vector<int> &nums, int begin, int end) {
+        //pivot 标杆位置 counter 小于poivt的元素的个数
+        int pivot = end, counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (nums[i] < nums[pivot]) {
+                swap(nums[i], nums[counter]);
+                counter++;
+            }
+        }
+        swap(nums[pivot], nums[counter]);
+        return counter;
     }
+    void quickSort(vector<int> &nums, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(nums, begin, end);
+        quickSort(nums, begin, pivot - 1);
+        quickSort(nums, pivot + 1, end);
+    }
+    
 };
 
 int main(int argc, const char * argv[]) {
@@ -35,9 +53,9 @@ int main(int argc, const char * argv[]) {
     
     Solution solu;
     vector<int> vec = {2,4,1,23,5,76,0,43,24,65};
-    vector<int> result = solu.sortArray(vec);
-    for (int i = 0; i < result.size(); i++) {
-        cout<<result[i]<<" ";
+    solu.quickSort(vec, 0, (int)vec.size() - 1);
+    for (int i = 0; i < vec.size(); i++) {
+        cout<<vec[i]<<" ";
     }
     
     cout<<endl;
